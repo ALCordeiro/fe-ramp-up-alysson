15ff2a70f0886d5b48eae1804186d4e85b8313c2
ALCordeiro <acordeiro@daitangroup.com>
2018-10-24 14:30:22 -0200

    professional skills with array dynamic

--- src/app/professional-skills/professional-skills.component.ts
+++ src/app/professional-skills/professional-skills.component.ts
@@ -7,6 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfessionalSkillsComponent implements OnInit {
 
+  array = [
+    {
+      id:1,
+      name: 'Angular',
+      value: 80
+    },
+    {
+      id:2,
+      name: 'Typescript',
+      value: 10
+    },
+    {
+      id:3,
+      name: 'Javascript',
+      value: 60
+    },
+    {
+      id:4,
+      name: 'HTML',
+      value: 70
+    },
+    {
+      id:5,
+      name: 'CSS',
+      value: 50
+    },
+    {
+      id:6,
+      name: 'SQL',
+      value: 40
+    },
+    {
+      id:7,
+      name: 'C#',
+      value: 20
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
