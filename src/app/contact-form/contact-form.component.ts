3119bc9a5920ddcb07a86757b9c1778d7f3d2eb7
ALCordeiro <acordeiro@daitangroup.com>
2018-10-25 22:40:29 -0200

    hotfixes

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -9,6 +9,34 @@ export class ContactFormComponent implements OnInit {
 
   @HostBinding('style.display') display = 'block';
 
+  array = [
+    {
+      id:1,
+      name: 'Email',
+      value: 'max@hotmail.com'
+    },
+    {
+      id:2,
+      name: 'Phone',
+      value: '+11 111111111'
+    },
+    {
+      id:3,
+      name: 'Viber',
+      value: '+11 111111111'
+    },
+    {
+      id:4,
+      name: 'Skype',
+      value: 'maxizin'
+    },
+    {
+      id:5,
+      name: 'Address',
+      value: 'Dog Street 420'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
