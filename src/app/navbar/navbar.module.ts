b64ab1cfb9b72fc8f5ee180d1505ec17a841fcfe
Alysson Lopes Cordeiro <acordeiro@brcpqd0039.daitan.com>
2018-10-30 17:09:51 -0300

    hotfix

--- src/app/navbar/navbar.module.ts
+++ src/app/navbar/navbar.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NavbarComponent } from './navbar.component';
+import { routing } from '../app.routing';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    routing
   ],
   declarations: [
     NavbarComponent
