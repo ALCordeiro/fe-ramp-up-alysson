250aa8e63a701d3c4dc9ec3c17def482b87f38cd
ALCordeiro <acordeiro@daitangroup.com>
2018-11-01 10:44:03 -0200

    hotfixes

--- src/app/main/main.routing.module.ts
+++ src/app/main/main.routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from '@angular/router';
 
-import { CardComponent } from './card/card.component';
-import { ProfessionalSkillsComponent } from './professional-skills/professional-skills.component';
-import { ContactFormComponent } from './contact-form/contact-form.component';
+// import { CardComponent } from './card/card.component';
+// import { ProfessionalSkillsComponent } from './professional-skills/professional-skills.component';
+// import { ContactFormComponent } from './contact-form/contact-form.component';
 
 const mainRoutes: Routes = [
     { path: 'about', loadChildren: '../main/card/card.module#CardModule' },
