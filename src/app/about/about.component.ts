04bf4055b464f9bbb66dbf01af7bdbc414d0b710
Alysson Lopes Cordeiro <acordeiro@brcpqd0039.daitan.com>
2018-11-06 14:06:18 -0200

    changes in service

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,28 +1,53 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
 import { InformationsService } from 'src/app/core/services/informations/informations.service';
-
-
-@Component({
+import { Highlight } from 'src/app/shared/enums/highlight.enum';
+import { Type } from 'src/app/shared/enums/type.enum';
+ @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
 
-  @HostBinding('style.display') display = 'block';
+    @HostBinding('style.display') display = 'block';
 
-  array = [];
+    about = [{
+      label: 'Age',
+      value: this.informationsService.informations.age,
+      type: Type.Date,
+      highlight: Highlight.NotHighlight
+    }, {
+      label: 'Adress',
+      value: this.informationsService.informations.address,
+      type: Type.Default,
+      highlight: Highlight.NotHighlight
+    }, {
+      label: 'Email',
+      value: this.informationsService.informations.email,
+      type: Type.Default,
+      highlight: Highlight.NotHighlight
+    }, {
+      label: 'Phone',
+      value: this.informationsService.informations.phone,
+      type: Type.Default,
+      highlight: Highlight.NotHighlight
+    }, {
+      label: 'Freelance',
+      value: this.informationsService.informations.freelance,
+      type: Type.Default,
+      highlight: Highlight.NotHighlight
+    }, {
+      label: 'Vacation',
+      value: this.informationsService.informations.vacation,
+      type: Type.Default,
+      highlight: Highlight.Highlight
+    }
+    ]
 
-  constructor(private informationsService: InformationsService) {
-  }
+    constructor(private informationsService: InformationsService) {
+    }
 
-  ngOnInit() {
-    this.array = this.informationsService.getInformations();
-  }
+    ngOnInit() {
+    }
 
-  filterItemsOfIndex(index){
-    var array = this.array.filter(x => !index.includes(x.index));
-    return array;
-  }
-
-}
+ }
--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
 import { InformationsService } from 'src/app/core/services/informations/informations.service';
-
-
-@Component({
+import { Type } from 'src/app/shared/enums/type.enum';
+import { Highlight } from 'src/app/shared/enums/highlight.enum';
+ @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss']
@@ -11,17 +11,38 @@ export class ContactFormComponent implements OnInit {
 
   @HostBinding('style.display') display = 'block';
 
-  array = [];
-
+  contact = [{
+    label: 'E-mail',
+    value: this.informationsService.informations.email,
+    type: Type.Default,
+    highlight: Highlight.NotHighlight
+  }, {
+    label: 'Phone',
+    value: this.informationsService.informations.phone,
+    type: Type.Default,
+    highlight: Highlight.NotHighlight
+  }, {
+    label: 'Viber',
+    value: this.informationsService.informations.viber,
+    type: Type.Default,
+    highlight: Highlight.NotHighlight
+  }, {
+    label: 'Skype',
+    value: this.informationsService.informations.skype,
+    type: Type.Default,
+    highlight: Highlight.NotHighlight
+  }, {
+    label: 'Adress',
+    value: this.informationsService.informations.address,
+    type: Type.Default,
+    highlight: Highlight.NotHighlight
+  }
+  ]
+  
   constructor(private informationsService: InformationsService) {
   }
-
+  
   ngOnInit() {
-    this.array = this.informationsService.getInformations();
   }
 
-  filterItemsOfIndex(index){
-    var array = this.array.filter(x => !index.includes(x.index));
-    return array;
-  }
 }
