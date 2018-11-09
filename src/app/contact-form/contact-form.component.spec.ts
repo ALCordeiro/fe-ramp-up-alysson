f28dbe13eaa83fa918db66bc9e05771802ce94f5
Alysson Lopes Cordeiro <acordeiro@brcpqd0039.daitan.com>
2018-11-09 15:19:10 -0200

    test for component

--- src/app/contact-form/contact-form.component.spec.ts
+++ src/app/contact-form/contact-form.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 import { InformationsService } from './../core/services/informations/informations.service';
 import { ContactFormComponent } from './contact-form.component';
--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,19 +1,25 @@
 import { RouterTestingModule } from '@angular/router/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 import { LoginService } from './../core/services/login/login.service';
 import { LoginComponent } from './login.component';
+import { User } from './user';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let service: LoginService;
+  let user: User;
+  let el: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
       imports: [FormsModule, RouterTestingModule],
-      providers: [LoginService]
+      providers: [LoginService, User]
     })
     .compileComponents();
   }));
@@ -21,7 +27,20 @@ describe('LoginComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    service = TestBed.get(LoginService);
+    user = TestBed.get(User);
     fixture.detectChanges();
+  });
+  
+  it('when method makeLogin() is called',() => {
+    spyOn(component, 'makeLogin').and.callThrough();
+    spyOn(service, 'makeLogin');
+
+    el = fixture.debugElement.query(By.css('.button'));
+    el.triggerEventHandler('click', null);
+
+    expect(component.makeLogin).toHaveBeenCalled();
+    expect(service.makeLogin).toHaveBeenCalledWith(user);
   });
 
   it('should create', () => {
