83d0fcedc044f6795a0ff842cfc455035d00964a
Alysson Lopes Cordeiro <acordeiro@brcpqd0039.daitan.com>
2018-11-12 10:55:46 -0200

    test pipes

--- src/app/shared/pipes/age-transform.pipe.spec.ts
+++ src/app/shared/pipes/age-transform.pipe.spec.ts
@@ -1,6 +1,25 @@
+import { TestBed, async } from '@angular/core/testing';
 import { AgeTransformPipe } from './age-transform.pipe';
 
 describe('AgeTransformPipe', () => {
+
+  let pipe: AgeTransformPipe;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      providers: [AgeTransformPipe]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    pipe = TestBed.get(AgeTransformPipe);
+  });
+
+  it('transform date in age', () => {
+    expect(pipe.transform('1996-08-30T03:00:00.000Z')).toBe('22');
+  });
+
   it('create an instance', () => {
     const pipe = new AgeTransformPipe();
     expect(pipe).toBeTruthy();
